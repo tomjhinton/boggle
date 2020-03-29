fc93b1a6f404c9702f4554f8034a7ba6b7c9487f
Tom Hinton <tomjhinton@gmail.com>
2020-03-29 00:08:38 +0000

    working

--- src/components/Main.js
+++ src/components/Main.js
@@ -52,7 +52,7 @@ class Main extends React.Component{
   mouseDown(e){
 
     this.setState({click: true})
-    if(e.target.innerText.length===1){
+    if(e.target.innerText.length===1 && this.state.playing){
       this.setState({selected: [e.target.innerText], selectedKey: [e.target.id]})
       e.target.classList.add('selected')
 
@@ -62,6 +62,7 @@ class Main extends React.Component{
   }
 
   mouseUp(){
+    if(this.state.playing){
     this.setState({click: false, selectedKey: []})
     const els = document.querySelectorAll('.letter')
     for (let i = 0; i < els.length; i++) {
@@ -71,18 +72,28 @@ class Main extends React.Component{
 
     if(dictionary.check(this.state.selected.join('').toLowerCase()) && this.state.selected.join('').toLowerCase().length >=2 && !this.state.played.includes(this.state.selected.join('').toLowerCase()) && this.state.playing){
       this.setState({score: [parseInt(this.state.score)+this.state.selected.join('').length], played: [...this.state.played, this.state.selected.join('').toLowerCase()]})
-      console.log('score')
+      // console.log('score')
     }
+  }
 
   }
 
   mouseOver(e){
-    if(this.state.click){
-      if(!e.target.classList.contains('selected')){
+
+    if(this.state.click && this.state.selectedKey.length>0 &&  this.state.playing){
+      const lastR = parseInt(this.state.selectedKey[this.state.selectedKey.length-1].split(':')[0])
+      const lastC = parseInt(this.state.selectedKey[this.state.selectedKey.length-1].split(':')[1])
+      const thisR = parseInt(e.target.id.split(':')[0])
+      const thisC = parseInt(e.target.id.split(':')[1])
+
+      console.log(lastC)
+      console.log(thisC)
+      if(!e.target.classList.contains('selected') && this.state.playing && (lastR === thisR || lastR === thisR-1 || lastR === thisR+1) && (lastC === thisC || lastC === thisC-1 || lastC === thisC+1 )){
+
         this.setState({selected: [...this.state.selected,e.target.innerText], selectedKey: [e.target.id]})
         e.target.classList.add('selected')
 
-        console.log(this.state.selectedKey)
+        // console.log(this.state.selectedKey)
       }
     }
 
@@ -101,11 +112,11 @@ class Main extends React.Component{
 
   select(e){
     e.persist()
-    console.log(e.target.innerText)
+    // console.log(e.target.innerText)
   }
   reset(){
     this.create()
-    this.setState({score: 0, time: 30, playing: true })
+    this.setState({score: 0, time: 60, playing: true, played: [] })
 
     setTimeout(() => {
       this.timer(), 10000
@@ -129,26 +140,35 @@ class Main extends React.Component{
   render() {
 
 
-    console.log(this.state)
+    // console.log(this.state)
     return (
 
       <div onMouseDown={this.mouseDown} onMouseUp={this.mouseUp} className="body">
         <div className='score'>Score: {this.state.score}</div>
         {!this.state.playing && <div className='reset' onClick={this.reset}> RESET</div>}
         {this.state.playing && <div className='timer'> {this.state.time}</div>}
-        <div className='board'>
+        <div className='game'>
+          <div className='board'>
 
-          {arr.map((x, row)=>{
-            return(
-              x.map((y, index)=>{
-                return(
-                  <div key={row+index}
-                    id={row+':'+index} className={'letter' + (this.state.selectedKey.includes(this.id) ? 'selected' : '')} onMouseEnter={this.mouseOver}>{y}</div>
-                )
-              }))
-          })}
+            {arr.map((x, row)=>{
+              return(
+                x.map((y, index)=>{
+                  return(
+                    <div key={row+index}
+                      id={row+':'+index} className={'letter' + (this.state.selectedKey.includes(this.id) ? 'selected' : '')} onMouseEnter={this.mouseOver}>{y}</div>
+                  )
+                }))
+            })}
+          </div>
+          <ul>
+            {this.state.played && this.state.played.map((x, index)=> {
+              return(
+                <li key={index}>{x}</li>
+              )
+            })}
+          </ul>
+
         </div>
-
       </div>
 
 
